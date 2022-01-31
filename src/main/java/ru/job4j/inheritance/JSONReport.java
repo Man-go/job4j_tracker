92869dd42476a26b4c119a196574bd86f49796aa
Severin <insmer@gmail.com>
2022-01-31 20:08:32 +0500

    Переопределение

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,15 +2,9 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
 
-    public static void main(String[] args) {
-
-        JSONReport jsonReport = new JSONReport();
-        System.out.println(jsonReport.generate("Report's name", "Report's body"));
-    }
-
     public String generate(String name, String body) {
         String ln = System.lineSeparator();
         return "{" + ln + "\t\"name\" : " + "\"" + name + "\"" + ","
-                + ln + "\t\"body\" : " + "\""+ body + "\"" + ln + "}";
+                + ln + "\t\"body\" : " + "\"" + body + "\"" + ln + "}";
     }
 }
