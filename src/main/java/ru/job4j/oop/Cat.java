88cdc6569cb978beba7570be6d7510e88cbdd4f2
Severin <insmer@gmail.com>
2022-01-31 13:53:21 +0500

    Вызов метода с возвращаемым типом

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
+    }
+
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
+}
