d1799cf851250202a70625ee29e93c2b6a007c9d
Severin <insmer@gmail.com>
2022-01-31 20:51:44 +0500

    Date. Отображение даты

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,6 +9,4 @@ public class StartUI {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(formatter));
     }
-
-
 }
