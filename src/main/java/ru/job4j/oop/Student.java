fe5b3404cf047d6600c7e08b1d3b6f85b9b0a067
Severin <insmer@gmail.com>
2022-01-31 13:37:25 +0500

    создание класса и методов

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -4,13 +4,16 @@ public class Student {
 
     public static void main(String[] args) {
         Student petya = new Student();
-        String song = "I believe, I can fly";
-        petya.music(song);
-
+        petya.music();
+        petya.song();
+        petya.music();
+        petya.song();
+        petya.music();
+        petya.song();
     }
 
-    public void music(String lyrics) {
-        System.out.println("I can sing a song : " + lyrics);
+    public void music() {
+        System.out.println("Tra tra tra");
     }
 
     public void song() {
