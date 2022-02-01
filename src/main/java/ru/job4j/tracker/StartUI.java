ea5ff4a0602f5dbe9b7dbb5361eafae281ec33a9
Severin <insmer@gmail.com>
2022-02-01 23:24:21 +0500

    Реализация класса StartUI. Вывод всех заявок

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -32,8 +32,7 @@ public class StartUI {
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
                 }
-            }
-            else if (select == 6) {
+            } else if (select == 6) {
                 run = false;
             }
         }
