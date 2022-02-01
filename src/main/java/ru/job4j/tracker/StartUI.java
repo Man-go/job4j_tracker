6ec0e8fad98defab4a43dab94045703513f089ea
Severin <insmer@gmail.com>
2022-02-01 19:45:38 +0500

    toString

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,10 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item(10, "BimBop");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(item.getCreated().format(formatter));
+
+        System.out.println(item);
     }
 }
