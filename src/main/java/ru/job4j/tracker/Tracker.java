d9d82252a04dc76dd318d92e70abb632c6a89898
Severin <insmer@gmail.com>
2022-02-02 10:58:18 +0500

    Метод удаления заявки Tracker.delete

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,14 +56,14 @@ public class Tracker {
     }
 
     public boolean delete(int id) {
+        boolean result = false;
         int index = indexOf(id);
         if (index != -1) {
             System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return true;
-        } else {
-            return false;
+            result = true;
         }
+        return result;
     }
 }
